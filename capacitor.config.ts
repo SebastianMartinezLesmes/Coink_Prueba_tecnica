228287e8c05e1944f4e81b2f0b8dd9f82630fc59
Lesmes <20rasputin03@gmail.com>
2024-06-24 12:11:13 -0500

    IMPLEMENT: Environment to develop the APK with Capacitor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'coink_evalue_test',
+  appName: 'ionic-app-base',
   webDir: 'www'
 };
 
